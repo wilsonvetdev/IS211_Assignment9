0a8ebbb9a9ffec3ae628588d87efa79f34d11b63
wilsonvetdev <wilsongch92@gmail.com>
2021-03-26 22:42:58 -0400

    apple stock dnot working

---
+++ apple_stock.py
@@ -0,0 +1,15 @@
+import requests 
+from bs4 import BeautifulSoup
+from pprint import pprint
+
+url = 'https://www.nasdaq.com/market-activity/stocks/aapl/historical'
+
+page_response = requests.get(url)
+
+# page = page_response.content
+
+# soup = BeautifulSoup(page, 'html.parser')
+
+# print(soup)
+
+print(page_response.status_code) # hung up on terminal, doesn't work and had to interrupt with keyboard.
